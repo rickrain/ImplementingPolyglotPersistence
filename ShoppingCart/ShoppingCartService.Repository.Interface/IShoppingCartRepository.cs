86d6d547fc89ef19c1503c4266de7260041a7501
rickrain <rickrain@hotmail.com>
2013-09-04 03:15:48 -0700

    Upgraded project to VS 2013.  Added shopping cart model and interface definition.  Some restructuring of solution to accommodate additional database categories and services.

---
+++ ShoppingCart/ShoppingCartService.Repository.Interface/IShoppingCartRepository.cs
@@ -0,0 +1,20 @@
+﻿using ShoppingCartService.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartService.Repository.Interface
+{
+    public interface IShoppingCartRepository
+    {
+        void AddItem(Item item);
+
+        void UpdateItem(Item item);
+
+        void RemoveItem();
+
+        List<Item> GetItems();
+    }
+}
